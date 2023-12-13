05967e193e919b04884512bc0f1ea9dcbf83adb2
HnatKruk <hnatkivpublic@gmail.com>
2023-12-13 10:12:24 +0100

    Updates

--- src/ mocks/index.ts
+++
@@ -1,1 +0,0 @@
-export { COUNTRIES } from './countries';
--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export { App } from './App';
-export { Form } from './Form';
-export { Table } from './Table';
-export { InputItem } from './InputItem';
-export { CheckboxItem } from './CheckboxItem';
-export { SelectItem } from './SelectItem';
-export { RadioItem } from './RadioItem';
